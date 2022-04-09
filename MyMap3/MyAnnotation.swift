f7abea55316867a94b1364f0de115c28241aff9f
Jack Shieh <71810019+sme322-ui@users.noreply.github.com>
2022-04-09 16:08:22 +0800

    Create MyAnnotation.swift

---
+++ MyMap3/MyAnnotation.swift
@@ -0,0 +1,16 @@
+
+import Foundation
+import MapKit
+
+class MyAnnotation: NSObject,MKAnnotation{
+    var coordinate:CLLocationCoordinate2D
+    var title: String?
+    var subtitle: String?
+    
+    init(coordinate: CLLocationCoordinate2D){
+        self.coordinate = coordinate
+    }
+    
+    
+    
+}
