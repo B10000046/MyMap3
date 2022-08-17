45f7e5b403c48c5cf3b7201bf29f3794be4f0d52
Jack Shieh <71810019+sme322-ui@users.noreply.github.com>
2022-08-17 20:17:36 +0800

    Update AppDelegate.swift
    Signed-off-by: Jack Shieh <71810019+sme322-ui@users.noreply.github.com>

--- MyMap3/AppDelegate.swift
+++ MyMap3/AppDelegate.swift
@@ -1,5 +1,5 @@
 import UIKit
-
+import Firebase
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -7,11 +7,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
     // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
