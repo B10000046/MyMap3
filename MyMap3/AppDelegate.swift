e2e3673921f2901a95b3f4859bb7342a74daae18
Jack Shieh <71810019+sme322-ui@users.noreply.github.com>
2022-08-18 21:04:02 +0800

    Update AppDelegate.swift
    Signed-off-by: Jack Shieh <71810019+sme322-ui@users.noreply.github.com>

--- MyMap3/AppDelegate.swift
+++ MyMap3/AppDelegate.swift
@@ -1,13 +1,19 @@
 import UIKit
 import Firebase
+import GoogleSignIn
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+    }
+    
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        GIDSignIn.sharedInstance()?.clientID = "332424414607-u1qa6vl72cus69j07mfrquaov04bg8t5.apps.googleusercontent.com"
+               
         return true
     }
 
@@ -24,5 +30,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        let sourceApplication = options[.sourceApplication] as? String
+        let annotation = options[.annotation]
+        
+        return GIDSignIn.sharedInstance()?.handle(url, sourceApplication: sourceApplication, annotation: annotation) ?? false
+    }
 }
