e834de3832db53eb4ff5f2189ba272e982564d8e
Jack Shieh <71810019+sme322-ui@users.noreply.github.com>
2022-08-21 18:03:35 +0800

    Update AppDelegate.swift
    Signed-off-by: Jack Shieh <71810019+sme322-ui@users.noreply.github.com>

--- MyMap3/AppDelegate.swift
+++ MyMap3/AppDelegate.swift
@@ -3,10 +3,41 @@ import Firebase
 import GoogleSignIn
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
-    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
-    }
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!,
+                 withError error: Error!) {
+           if let error = error {
+               print("\(error.localizedDescription)")
+               // [START_EXCLUDE silent]
+               NotificationCenter.default.post(
+                   name: Notification.Name(rawValue: "ToggleAuthUINotification"), object: nil, userInfo: nil)
+               // [END_EXCLUDE]
+           } else {
+               // Perform any operations on signed in user here.
+               let userId = user.userID                  // For client-side use only!
+               let idToken = user.authentication.idToken // Safe to send to the server
+               let fullName = user.profile.name
+               let givenName = user.profile.givenName
+               let familyName = user.profile.familyName
+               let email = user.profile.email
+               // [START_EXCLUDE]
+               NotificationCenter.default.post(
+                   name: Notification.Name(rawValue: "ToggleAuthUINotification"),
+                   object: nil,
+                   userInfo: ["statusText": "Signed in user:\n\(fullName)"])
+               // [END_EXCLUDE]
+           }
+       }
     
-
+    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!,
+                  withError error: Error!) {
+            // Perform any operations when the user disconnects from app here.
+            // [START_EXCLUDE]
+            NotificationCenter.default.post(
+                name: Notification.Name(rawValue: "ToggleAuthUINotification"),
+                object: nil,
+                userInfo: ["statusText": "User has disconnected."])
+            // [END_EXCLUDE]
+        }
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
@@ -30,10 +61,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        let sourceApplication = options[.sourceApplication] as? String
-        let annotation = options[.annotation]
-        
-        return GIDSignIn.sharedInstance()?.handle(url, sourceApplication: sourceApplication, annotation: annotation) ?? false
-    }
+    @available(iOS 9.0, *)
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+           return GIDSignIn.sharedInstance().handle(url,
+                                                    sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+                                                    annotation: options[UIApplication.OpenURLOptionsKey.annotation])
+       }
 }
