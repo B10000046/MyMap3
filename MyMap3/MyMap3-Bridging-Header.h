0b17293e309b8d0a846733e20e193a6a807590a9
Jack Shieh <71810019+sme322-ui@users.noreply.github.com>
2023-01-02 19:24:12 +0800

    Create MyMap3-Bridging-Header.h
    Signed-off-by: Jack Shieh <71810019+sme322-ui@users.noreply.github.com>

---
+++ MyMap3/MyMap3-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+// 呼叫 C++ 匯入 Objective-C 標頭檔案
+
+#import "HelloWorldWrapper.h"
+#import <CoreBluetooth/CoreBluetooth.h>
